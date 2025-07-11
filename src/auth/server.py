01581ac5e372438873c63d15cd57406f62492ff1
saikumaradapa <saikumaradapa3266@gmail.com>
2025-07-11 18:49:22 +0530

    add docker compose

--- src/auth/server.py
+++ src/auth/server.py
@@ -42,7 +42,7 @@ def login():
         return "invalid credentials", 401
 
 
-@server.route("/validate", method=["POST"])
+@server.route("/validate", methods=["POST"])
 def validate():
     encoded_jwt = request.headers["Authorization"]
 
