4d3eb8a701b34f96576e0b3a81db05b431ad7a79
saikumaradapa <saikumaradapa3266@gmail.com>
2025-07-11 19:30:01 +0530

    update auth server

--- src/auth/server.py
+++ src/auth/server.py
@@ -3,31 +3,29 @@ from flask import Flask, request
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 
-load_dotenv(".env")
-
 server = Flask(__name__)
 mysql = MySQL(server)
+load_dotenv(".env")
 
-# Load all config
+# config
 server.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.getenv("MYSQL_PORT")
+server.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
+
 
 @server.route("/login", methods=["POST"])
 def login():
     auth = request.authorization
     if not auth:
         return "missing credentials", 401
-    
-    # check DB for username and password 
+
+    # check db for username and password
     cur = mysql.connection.cursor()
     res = cur.execute(
         "SELECT email, password FROM user WHERE email=%s", (auth.username,)
-        )
-
-
+    )
 
     if res > 0:
         user_row = cur.fetchone()
@@ -37,7 +35,7 @@ def login():
         if auth.username != email or auth.password != password:
             return "invalid credentials", 401
         else:
-            return createJWT(auth.username, os.getenv("JWT_SECRET"), True)
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
     else:
         return "invalid credentials", 401
 
@@ -53,7 +51,7 @@ def validate():
 
     try:
         decoded = jwt.decode(
-            encoded_jwt, os.getenv("JWT_SECRET"), algorithms=["HS256"]
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
         )
     except:
         return "not authorized", 403
@@ -61,14 +59,12 @@ def validate():
     return decoded, 200
 
 
-
-
 def createJWT(username, secret, authz):
     return jwt.encode(
         {
             "username": username,
-            "exp": datetime.datetime.now(tz=datetime.timezone.utc) +
-            datetime.timedelta(days=1),
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
             "iat": datetime.datetime.utcnow(),
             "admin": authz,
         },
@@ -76,5 +72,6 @@ def createJWT(username, secret, authz):
         algorithm="HS256",
     )
 
+
 if __name__ == "__main__":
-    server.run(host="localhost", port=5000)
+    server.run(host="0.0.0.0", port=5000, debug=True)
